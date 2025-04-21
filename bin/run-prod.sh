a8c8aae74e8c14fd2e931a2c03e7f6592e214594
Ander Linares <ander.linares@outlook.com>
2025-04-21 15:22:55 -0500

    little change

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -2,5 +2,6 @@
 
 COMPOSE_PROVIDER="${COMPOSE_PROVIDER:-docker compose}"
 
+echo "### Script executed at: $(date '+%Y-%m-%d %H:%M:%S')"
 exec ${COMPOSE_PROVIDER} -f docker-compose.yml run -T --rm jr-insc
 
